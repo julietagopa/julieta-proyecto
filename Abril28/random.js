a958f45f8f46262ab5e92bdece5ed4e93c3a5c7f
unknown <julietagopa2@gmail.com>
2020-04-30 20:10:45 -0500

    Hoy

---
+++ Abril28/random.js
@@ -0,0 +1,22 @@
+function inicio() {
+  let numeroUno = prompt("Inserta el numero uno");
+  let numeroDos = prompt("Inserta el numero dos");
+  let operacion = prompt ("sumar, restar o multiplicar");
+
+if (operacion=="sumar") {
+  let resultado = parseInt(numeroUno)+ parseInt(numeroDos);
+  alert(resultado);
+ if (operacion == "multiplicar") {
+    let resultado = parseInt(numeroUno)* parseInt(numeroDos);
+    alert (resultado);
+ if (operacion=="restar") {
+    let resultado =parseInt(numeroUno)- parseInt(numeroDos);
+    alert (resultado);
+    if {
+      alert("ERROR")
+      }
+    }
+
+      }
+    }
+}
--- java script/funciones.js
+++ java script/funciones.js
@@ -1,4 +1,43 @@
-function cambiarcolor() {
- let etiqueta = document.getElementById('aqui')
- etiqueta.classlist.add("letrasRojas")
+
+
+let coloreado =false
+let subrayado = false
+
+
+function pedirNombre(){
+  let nombre =prompt ("tu nombre")
+  document.getElementById('aqui').innerHTML= "Hola" + nombre
 }
+
+function cambiarcolor() {
+
+ let etiqueta = document.getElementById("aqui")
+ etiqueta.classList.add("letrasRojas")
+ //etiqueta.style.color="red"
+
+}
+
+function colorear(){
+  let texto =document.getElementById("aqui")
+
+  if(coloreado ==false) {
+    texto.classList.add("letrasRojas")
+    coloreado = true
+  } else {
+    texto.classList.remove("letrasRojas")
+    coloreado = false
+
+  }
+}
+
+function subrayar(){
+  let texto = document.getElementById("aqui")
+
+    if(subrayado ==false){
+      texto.classList.add("subrayar")
+      subrayado = true
+    } else {
+      texto.classList.add("subrayar")
+      subrayado = false
+    }
+    }

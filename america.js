4159ec464f7ec6f024ba7bdba53adeec6ad8dbbe
Regina-a <gomezpr.2005@gmail.com>
2020-12-03 16:02:02 -0600

    tribilin

---
+++ america.js
@@ -0,0 +1,10 @@
+
+function mostrar(elid) {
+    console.log ('id seleccionado', elid)
+    document.getElementById(elid).style.display = 'inline-block';
+}
+    
+    function ocultar(){
+    document.getElementById('ja').style.display = 'none';
+}
+    

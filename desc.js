a970aaa274edea916f0399869cca7adc58abff7b
unknown <julietagopa2@gmail.com>
2020-12-15 14:16:56 -0600

    final

--- desc.js
+++ desc.js
@@ -1,12 +1,15 @@
-result = window.confirm("Quieres decubir?");{
-    if (result != null)
-    
-       var links = new Array( "index.html", "who.html", src="artistadelasemana.html", "file:///C:/Users/lenovo/Desktop/julieta-proyecto/Generos.html","file:///C:/Users/lenovo/Desktop/julieta-proyecto/REGINA.html" , "http://www.altavista.com", "http://www.hotbot.com", "http://www.buscopio.com", "http://www.sol.es", "http://www.excite.com", "http://www.elbuscador.com", "http://www.ya.com", "http://www.wanadoo.es", "http://www.buscador.com.mx", "http://www.msn.com", "http://www.astrolabio.net");
+/*result = window.confirm("Quieres decubir?");*/
+   
+        if (window.confirm("¿TE GUSTARÍA DESCUBRIR ALGO NUEVO?  Haz click en OK" )) {
+            /*window.open("exit.html", "Thanks for Visiting!");*/
+            var links = new Array( "./REGINA.html", "./who.html", "./artistadelasemana.html","./Generos.html", "./nuevosartistas.html", "./GALERIADEARTISTAS.html", "./TRENDING.html" );
+            enlaceAleatorio()
+          }
+       
+
         function enlaceAleatorio(){
+            console.log("random")
            aleat = Math.random() * links.length;
            aleat = Math.floor(aleat);
            window.location=links[aleat];
         }
-    
-    }
-    
